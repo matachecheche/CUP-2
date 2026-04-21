cfc2f3378175c45a69886ed48b66cb3457b68f35
matachecheche <49599299+matachecheche@users.noreply.github.com>
2026-04-21 23:16:45 +0000

    model session agregado

---
+++ app/Models/Session.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Session extends Model
+{
+    protected $table = 'sessions';
+    protected $primaryKey = 'id';
+    public $incrementing = false;
+    public $timestamps = false;
+
+    protected $fillable = [
+        'id',
+        'user_id',
+        'ip_address',
+        'user_agent',
+        'payload',
+        'last_activity',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
